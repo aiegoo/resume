24b8f0a0c4c68de79d626630317e7aae8777bb39
aiegoo <eozz21@gmail.com>
2019-09-18 20:57:20 +0900

    fix conflicts of index.php

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
 <?php
 	
-<<<<<<< HEAD
 	header('Content-Type: text/html; charset=utf-8');
 	
 	ob_start("ob_gzhandler");
 	
-=======
->>>>>>> tmp
 	/* LOAD CONFIG */
 	require_once('config.inc.php');
 	
 	/* DEPENDENCIES */
-<<<<<<< HEAD
 	require_once('libraries/router.class.php');
 	require_once('libraries/orm.class.php');
 	require_once('libraries/helper.class.php');
-=======
-	require_once('services/router.class.php');
-	require_once('services/orm.class.php');
->>>>>>> tmp
 	
 	require_once('controllers/main.class.php');
 	require_once('controllers/view.class.php');
